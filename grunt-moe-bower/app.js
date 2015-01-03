e2726546c50a194df6bcea4c361757e1bc383372
Siqi Lu <lu.siqi@outlook.com>
2015-01-03 16:27:03 +0800

    add .jshintrc

--- grunt-moe-bower/app.js
+++ grunt-moe-bower/app.js
@@ -1,8 +1,8 @@
 ﻿/*
- * grunt-bower-task
- * https://github.com/yatskevich/grunt-bower-task
+ * grunt-moe-bower
+ * https://github.com/SiqiLu/grunt-moe-bower
  *
- * Copyright (c) 2012 Ivan Yatskevich
+ * Copyright (c) 2015 SiqiLu
  * Licensed under the MIT license.
  */
 
@@ -10,7 +10,6 @@
              
 var moePackage = function (grunt) {
 
-    grunt = this;
     var bower;
     var path;
     var async;
@@ -38,33 +37,33 @@ var moePackage = function (grunt) {
     function fail(error) {
         grunt.fail.fatal(error);
     }
-    
+
     function clean(dir, callback) {
         rimraf(dir);
         callback();
     }
-    
+
     function install(options, callback) {
         bower.commands.install([], options.bowerOptions)
-      .on('log', function (result) {
-            log(['bower', result.id.cyan, result.message].join(' '));
-        })
-      .on('error', fail)
-      .on('end', callback);
+            .on('log', function (result) {
+                log(['bower', result.id.cyan, result.message].join(' '));
+            })
+            .on('error', fail)
+            .on('end', callback);
     }
-    
+
     function copy(options, callback) {
         var bowerAssets = new bowerAssets(bower, options.cwd);
         bowerAssets.on('end', function (assets) {
             var copier = new assetCopier(assets, options, function (source, destination, isFile) {
                 log('grunt-bower ' + 'copying '.cyan + ((isFile ? '' : ' dir ') + source + ' -> ' + destination).grey);
             });
-            
+
             copier.once('copied', callback);
             copier.copy();
         }).get();
     }
-    
+
     grunt.registerMultiTask('bower', 'Install Bower packages.', function () {
         var tasks = [],
             done = this.async(),
@@ -88,49 +87,50 @@ var moePackage = function (grunt) {
             },
             bowerDir,
             targetDir;
-        
+
         // calling require on the dependencies has been delayed to prevent slow
         // dependencies delaying the startup of grunt even if this task is not used
         // at all
         requireDependencies();
-        
+
         bowerDir = path.resolve(bower.config.directory);
         targetDir = path.resolve(options.targetDir);
-        
+
         log.logger = options.verbose ? grunt.log : grunt.verbose;
         options.layout = layoutsManager.getLayout(options.layout, fail);
         options.cwd = grunt.option('base') || process.cwd();
-        
+
         if (options.cleanup !== undefined) {
             options.cleanTargetDir = options.cleanBowerDir = options.cleanup;
         }
-        
+
         if (options.cleanTargetDir) {
             add('Cleaned target dir ' + targetDir.grey, function (callback) {
                 clean(targetDir, callback);
             });
         }
-        
+
         if (options.install) {
             add('Installed bower packages', function (callback) {
                 install(options, callback);
             });
         }
-        
+
         if (options.copy) {
             add('Copied packages to ' + targetDir.grey, function (callback) {
                 copy(options, callback);
             });
         }
-        
+
         if (options.cleanBowerDir) {
             add('Cleaned bower dir ' + bowerDir.grey, function (callback) {
                 clean(bowerDir, callback);
             });
         }
-        
+
         async.series(tasks, done);
     });
 };
 
 module.exports = moePackage;
+console.log('hello word');
