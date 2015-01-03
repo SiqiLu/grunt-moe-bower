0e3873d82e54c50d62f24c5a8f9d75bb48894932
Siqi Lu <lu.siqi@outlook.com>
2015-01-03 13:47:23 +0800

    'ignore:201501031347'

---
+++ grunt-moe-bower/README.md
@@ -0,0 +1,3 @@
+﻿# grunt-moe-bower
+
+
---
+++ grunt-moe-bower/gruntfile.js
@@ -0,0 +1,18 @@
+﻿module.exports = function (grunt) {
+    
+    grunt.initConfig({
+        debug: {
+            options: {
+                open: true
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-debug-task');
+    grunt.loadNpmTasks('grunt-bower-task');
+
+    var plugin = require('./app.js');
+    grunt.registerTask('foo', 'A sample task that logs stuff.', apply(grunt, plugin));
+
+    
+};
