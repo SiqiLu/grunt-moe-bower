e01d1b15ea7ff864e0713e170b23d7103c92f68b
Siqi Lu <lu.siqi@outlook.com>
2015-03-18 10:38:18 +0800

    Release 0.1.3

--- tasks/moe-bower.js
+++ tasks/moe-bower.js
@@ -6,7 +6,7 @@
  * Licensed under the MIT license.
  */
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   'use strict';
 
   var path = require('path');
@@ -32,7 +32,7 @@ module.exports = function(grunt) {
 
   function install(options, callback) {
     bower.commands.install([], options.bowerOptions)
-      .on('log', function(result) {
+      .on('log', function (result) {
         log(['bower', chalk.cyan(result.id), result.message].join(' '));
       })
       .on('error', fail)
@@ -50,41 +50,39 @@ module.exports = function(grunt) {
 
     if (!grunt.file.exists(bowerDir)) {
       log('Can not find bower_components folder. Copy skipped.');
-      return;
+    } else {
+      _(fs.readdirSync(bowerDir)).uniq().remove(function (item) {
+        return !grunt.file.isDir(item) && grunt.file.exists(bowerDir, item, '.bower.json');
+      }).map(function (item) {
+        return path.join(bowerDir, item);
+      }).each(function (value) {
+        var bowerJson = grunt.file.readJSON(path.join(value, '.bower.json'));
+        var pkg = {
+          srcDir: value,
+          tarDir: path.join(targetDir, bowerJson.name + '@' + bowerJson.version),
+          files: []
+        };
+        grunt.file.recurse(value, function callback(abspath, rootdir, subdir, filename) {
+          pkg.files.push(path.join(subdir || '', filename));
+          tally.files++;
+        });
+        packages.push(pkg);
+        tally.pkgs++;
+      });
+
+      _(packages).each(function (pkg) {
+        _(pkg.files).each(function (file) {
+          grunt.file.copy(path.join(pkg.srcDir, file), path.join(pkg.tarDir, file));
+        });
+      });
+
+      log('Copied ' + chalk.cyan(tally.pkgs.toString()) + (tally.pkgs === 1 ? ' package' : ' packages') + ', and ' +
+        chalk.cyan(tally.files.toString()) + (tally.files === 1 ? ' file' : ' files.'));
     }
-
-    _(fs.readdirSync(bowerDir)).uniq().remove(function(item) {
-      return !grunt.file.isDir(item) && grunt.file.exists(bowerDir, item, '.bower.json');
-    }).map(function(item) {
-      return path.join(bowerDir, item);
-    }).each(function(value) {
-      var bowerJson = grunt.file.readJSON(path.join(value, '.bower.json'));
-      var pkg = {
-        srcDir: value,
-        tarDir: path.join(targetDir, bowerJson.name + '@' + bowerJson.version),
-        files: []
-      };
-      grunt.file.recurse(value, function callback(abspath, rootdir, subdir, filename) {
-        pkg.files.push(path.join(subdir || '', filename));
-        tally.files++;
-      });
-      packages.push(pkg);
-      tally.pkgs++;
-    });
-
-    _(packages).each(function(pkg) {
-      _(pkg.files).each(function(file) {
-        grunt.file.copy(path.join(pkg.srcDir, file), path.join(pkg.tarDir, file));
-      });
-    });
-
-    log('Copied ' + chalk.cyan(tally.pkgs.toString()) + (tally.pkgs === 1 ? ' package' : ' packages') + ', and ' +
-      chalk.cyan(tally.files.toString()) + (tally.files === 1 ? ' file' : ' files.'));
-
     callback();
   }
 
-  grunt.registerMultiTask('mbower', 'Install and distribute Bower packages.', function() {
+  grunt.registerMultiTask('mbower', 'Install and distribute Bower packages.', function () {
 
     var tasks = [];
     var done = this.async();
@@ -98,9 +96,9 @@ module.exports = function(grunt) {
       bowerOptions: {}
     });
 
-    var add = function(successMessage, fn) {
-      tasks.push(function(callback) {
-        fn(function() {
+    var add = function (successMessage, fn) {
+      tasks.push(function (callback) {
+        fn(function () {
           grunt.log.ok(successMessage);
           callback();
         });
@@ -110,25 +108,25 @@ module.exports = function(grunt) {
     var targetDir = options.targetDir = path.resolve(options.targetDir, './moe_packages');
 
     if (options.cleanTargetDir) {
-      add('Cleaned target dir ' + chalk.grey(targetDir), function(callback) {
+      add('Cleaned target dir ' + chalk.grey(targetDir), function (callback) {
         clean(targetDir, callback);
       });
     }
 
     if (options.install) {
-      add('Installed bower packages', function(callback) {
+      add('Installed bower packages', function (callback) {
         install(options, callback);
       });
     }
 
     if (options.copy) {
-      add('Copied packages to ' + targetDir.grey, function(callback) {
+      add('Copied packages to ' + targetDir.grey, function (callback) {
         copy(options, callback);
       });
     }
 
     if (options.cleanBowerDir) {
-      add('Cleaned bower dir ' + bowerDir.grey, function(callback) {
+      add('Cleaned bower dir ' + bowerDir.grey, function (callback) {
         clean(bowerDir, callback);
       });
     }
